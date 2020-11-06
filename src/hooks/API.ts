0eb3a061c78cc44a8b7baba6892419450c04fdf4
ygkn <2000ygkn0713@gmail.com>
2020-11-06 16:23:02 +0900

    refactor: extract fetcher to `fetcher` directory

--- src/hooks/API.ts
+++ src/hooks/API.ts
@@ -5,30 +5,14 @@ import {
   InfiniteQueryConfig,
 } from 'react-query';
 
+import { fetchAPI } from '../fetcher/API';
 import { QueryKey, QueryParams, QueryResult } from '../types/API';
-
-const queryAPI = async <Key extends QueryKey>(
-  key: Key,
-  params: QueryParams<Key>,
-
-  cursor: Partial<QueryParams<Key>> = {}
-) => {
-  const paramsString = Object.entries({ ...params, ...cursor })
-    .map(
-      ([paramsKey, paramsValue]) =>
-        `${encodeURIComponent(paramsKey)}=${encodeURIComponent(paramsValue)}`
-    )
-    .join('&');
-
-  const result = await fetch(`http://127.0.0.1:5000/${key}?${paramsString}`);
-  return result.json();
-};
 
 export const useQueryAPI = <Key extends QueryKey>(
   key: Key,
   params: QueryParams<Key>,
   config?: QueryConfig<QueryResult<Key>, Error>
-) => useQuery<QueryResult<Key>, Error>([key, params], queryAPI, config);
+) => useQuery<QueryResult<Key>, Error>([key, params], fetchAPI, config);
 
 export const useInfiniteQueryAPI = <Key extends QueryKey>(
   key: Key,
@@ -42,4 +26,4 @@ export const useInfiniteQueryAPI = <Key extends QueryKey>(
       allPages: QueryResult<Key>[]
     ) => boolean | Partial<QueryParams<Key>>;
   }
-) => useInfiniteQuery<QueryResult<Key>, Error>([key, params], queryAPI, config);
+) => useInfiniteQuery<QueryResult<Key>, Error>([key, params], fetchAPI, config);
