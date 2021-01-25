7844fc98350f33ac610a5c5a0926c5697611d1fb
ygkn <2000ygkn0713@gmail.com>
2021-01-26 06:56:34 +0900

    PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ import Document, {
   DocumentInitialProps,
 } from 'next/document';
 
+import manifestJSON from '../../public/manifest.json';
+
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
@@ -22,6 +24,17 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="UTF-8" />
           <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#B61F38" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="apple-mobile-web-app-title" content={manifestJSON.name} />
+          <meta name="application-name" content={manifestJSON.short_name} />
         </Head>
         <body className="font-sans bg-blue-50 text-gray-900 overflow-y-scroll">
           <Main />
